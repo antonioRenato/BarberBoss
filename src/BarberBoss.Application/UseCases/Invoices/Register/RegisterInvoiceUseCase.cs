13bb984742bce13c2f674c185066154571fd8478
antonioRenato <antoniorpfs@gmail.com>
2024-09-15 23:38:18 -0300

    Created validate

--- src/BarberBoss.Application/UseCases/Invoices/Register/RegisterInvoiceUseCase.cs
+++ src/BarberBoss.Application/UseCases/Invoices/Register/RegisterInvoiceUseCase.cs
@@ -4,12 +4,21 @@ using BarberBoss.Communication.Responses;
 namespace BarberBoss.Application.UseCases.Invoices.Register
 {
     public class RegisterInvoiceUseCase
-    {
+    {        
         public ResponseRegisterInvoiceJson Execute(RequestRegisterInvoiceJson request)
         {
-            // Validações
-            
+            Validate(request);
+
             return new ResponseRegisterInvoiceJson();
+        }
+
+        private void Validate(RequestRegisterInvoiceJson request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Title))
+                throw new ArgumentException("The title is required!");
+
+            if (request.Amount <= 0)
+                throw new ArgumentException("The amount must be greater than zero");
         }
     }
 }
